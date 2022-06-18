b6deac72906272ee14c75544ce0f0fc2e9d21236
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-06-18 15:35:41 -0400

    Admin and Employee functions

--- src/main/java/com/revature/models/Admin.java
+++ src/main/java/com/revature/models/Admin.java
@@ -1,10 +1,14 @@
 package com.revature.models;
 
-public class Admin extends User {
+public class Admin extends Employee {
 
     public Admin(String username, String password) {
         super(username, password);
-        
     }
     
+    public void transferFunds(Account ac1, Account ac2, double amt) {
+        ac1.withdraw(amt);
+        ac2.deposit(amt);
+    }
+
 }
--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -4,7 +4,15 @@ public class Employee extends User {
 
     public Employee(String username, String password) {
         super(username, password);
-        
+    }
+
+    public void approveAcc(Bank bank, int accIndex) {
+        Account approved = bank.popPending(accIndex);
+        bank.addApprovedAccs(approved);
+    }
+    
+    public void denyAcc(Bank bank, int accIndex) {
+        bank.popPending(accIndex);
     }
     
 }
