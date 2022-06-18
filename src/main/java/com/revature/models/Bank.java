fe52daf58de8a6192b6746006f8c6d3484342fc6
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-06-18 14:35:04 -0400

    fleshing out bank class functionality

--- src/main/java/com/revature/models/Bank.java
+++ src/main/java/com/revature/models/Bank.java
@@ -1,5 +1,70 @@
 package com.revature.models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Bank {
+
+    private List<Account> pendingAccs = new ArrayList<>();
+    private List<Account> approvedAccs = new ArrayList<>();
+    private HashMap<String, User> bankUsers = new HashMap<>();
+
+    public Bank() {}
     
+    public Bank(List<Account> pendingAccs, List<Account> approvedAccs, HashMap<String, User> bankUsers) {
+        this.pendingAccs = pendingAccs;
+        this.approvedAccs = approvedAccs;
+        this.bankUsers = bankUsers;
+    }
+
+    public List<Account> getPendingAccs() {
+        return pendingAccs;
+    }
+
+    public Account popPending(int index) {
+        return this.pendingAccs.remove(index);
+    }
+
+    public void setPendingAccs(List<Account> pendingAccs) {
+        this.pendingAccs = pendingAccs;
+    }
+
+    public void addPendingAccs(Account acc) {
+        this.pendingAccs.add(acc);
+    }
+    
+    public List<Account> getApprovedAccs() {
+        return approvedAccs;
+    }
+
+    public Account getApprovedAcc(int index) {
+        return this.approvedAccs.get(index);
+    }
+
+    public Account popApproved(int index) {
+        return this.approvedAccs.remove(index);
+    }
+    
+    public void setApprovedAccs(List<Account> approvedAccs) {
+        this.approvedAccs = approvedAccs;
+    }
+    
+    public void addApprovedAccs(Account acc) {
+        this.approvedAccs.add(acc);
+    }
+
+    public HashMap<String, User> getBankUsers() {
+        return bankUsers;
+    }
+
+    public void setBankUsers(HashMap<String, User> bankUsers) {
+        this.bankUsers = bankUsers;
+    }
+
+    public void addBankUser(String username, User user) {
+        this.bankUsers.put(username, user);
+    }
+    
+
 }
