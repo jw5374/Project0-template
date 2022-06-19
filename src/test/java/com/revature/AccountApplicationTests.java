9dc6f2f589df9a666b5015a6088c19f13d1b1033
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-06-19 16:22:44 -0400

    account application test formatting changes

--- src/test/java/com/revature/AccountApplicationTests.java
+++ src/test/java/com/revature/AccountApplicationTests.java
@@ -14,6 +14,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
     public void customerApplyForAccount() {
         Customer c1 = (Customer) bank.getBankUser("john2");
         c1.applyAccount(bank);
+
         Assert.assertEquals(1, bank.getPendingAccs().size());
     }
 
@@ -21,6 +22,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
     public void customerApplyForAccountUserAttachment() {
         Customer c1 = (Customer) bank.getBankUser("john2");
         c1.applyAccount(bank);
+
         Assert.assertEquals("User [email=jt@email.com, firstname=John, lastname=Thomas, phone=9876543210, username=john2]", bank.popPending(0).getAttachedUsers().get(0).toString());
         Assert.assertEquals(0, bank.getPendingAccs().size());
     }
@@ -33,6 +35,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
         bank.addApprovedAccs(applied);
         c1.addOpenAccount(applied);
         c1 = (Customer) bank.getBankUser("john2");
+
         Assert.assertEquals(0, bank.getPendingAccs().size());
         Assert.assertEquals(1, bank.getApprovedAccs().size());
         Assert.assertEquals(1, c1.getOpenAccounts().size());
@@ -45,6 +48,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
         Employee empl = (Employee) bank.getBankUser("jane1");
         cust.applyAccount(bank);
         empl.approveAcc(bank, 0, "john1");
+
         Assert.assertEquals(0, bank.getPendingAccs().size());
         Assert.assertEquals(1, bank.getApprovedAccs().size());
         Assert.assertEquals(1, cust.getOpenAccounts().size());
@@ -57,6 +61,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
         Admin adm = (Admin) bank.getBankUser("james1");
         cust.applyAccount(bank);
         adm.approveAcc(bank, 0, "john1");
+        
         Assert.assertEquals(0, bank.getPendingAccs().size());
         Assert.assertEquals(1, bank.getApprovedAccs().size());
         Assert.assertEquals(1, cust.getOpenAccounts().size());
@@ -69,6 +74,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
         Employee empl = (Employee) bank.getBankUser("jane1");
         cust.applyAccount(bank);
         empl.denyAcc(bank, 0);
+
         Assert.assertEquals(0, bank.getPendingAccs().size());
         Assert.assertEquals(0, bank.getApprovedAccs().size());
         Assert.assertEquals(0, cust.getOpenAccounts().size());
@@ -80,6 +86,7 @@ public class AccountApplicationTests extends TestBankFuncsSetup {
         Admin adm = (Admin) bank.getBankUser("james1");
         cust.applyAccount(bank);
         adm.denyAcc(bank, 0);
+        
         Assert.assertEquals(0, bank.getPendingAccs().size());
         Assert.assertEquals(0, bank.getApprovedAccs().size());
         Assert.assertEquals(0, cust.getOpenAccounts().size());
