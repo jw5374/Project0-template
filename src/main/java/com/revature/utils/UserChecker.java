af30979d30c38eee5ea051e98c2a4a451e5c5ec1
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-06-21 22:04:54 -0400

    added checker util for user not exists

--- src/main/java/com/revature/utils/UserChecker.java
+++ src/main/java/com/revature/utils/UserChecker.java
@@ -3,6 +3,7 @@ package com.revature.utils;
 import java.util.HashMap;
 
 import com.revature.exceptions.UserAlreadyExistsException;
+import com.revature.exceptions.UserDoesNotExistException;
 import com.revature.models.User;
 
 public class UserChecker {
@@ -10,6 +11,12 @@ public class UserChecker {
     public static void checkUsername(String username, HashMap<String, User> users) {
         if(users.containsKey(username)) {
             throw new UserAlreadyExistsException(username);
+        }
+    }
+
+    public static void checkUsernameNotExists(String username, HashMap<String, User> users) {
+        if(!users.containsKey(username)) {
+            throw new UserDoesNotExistException();
         }
     }
 
